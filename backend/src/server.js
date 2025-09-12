54dbcbed01e21cf13c968b1debad0a2e86bae618
marcus-kako <markosf.dev@gmail.com>
2025-09-12 14:05:56 -0300

    Adiciona Cors, para poder usar api no frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import routes from "./Routes/routes.js";
 import { configDotenv } from "dotenv";
 import { main } from "./db/conn.js";
+import cors from 'cors'
 configDotenv();
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes)
 
